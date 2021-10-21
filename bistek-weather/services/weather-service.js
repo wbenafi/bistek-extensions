d2d6a389aee6d9063aa845cb82c9a20cbc2f525c
Jefferson000 <jefersontorres000@gmail.com>
2021-10-21 17:27:17 -0600

    remove service

--- bistek-weather/services/weather-service.js
+++
@@ -1,9 +0,0 @@
-const getWeather = async () => {
-    // token = 'BBFF-lJaJsgcrMJULPFXzq65AYmjkpKlUHm'
-    // temperture_id = '6171df200bbdde19e474a2e4'
-    // const response = await fetch(`https://industrial.api.ubidots.com/api/v1.6/auth/token/?token=expressio${token}`);
-    // const data = await response.json
-    // console.log(data);
-    // return data;
-}
-getWeather()
