aa8fec36c4c82eb4cc06aeb207393f0c67ef912b
JoseOcon <josebermudez587@gmail.com>
2021-10-23 01:00:25 -0600

    twitter

--- twitter-extension/views/popup/popup.js
+++ twitter-extension/views/popup/popup.js
@@ -1,9 +1,58 @@
-const GetRestaurants = () => {
-  const body = fetch("https://jsonplaceholder.typicode.com/posts/1").then(
-    (res) => res.json()
-  );
+const makeRequest = (message) => {
+  const body = fetch("https://bistek-extensions.herokuapp.com/twitter/", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify({
+      message,
+    }),
+  });
 
   return body;
 };
 
-console.log(GetRestaurants())
+function createTweet() {
+  const tweetContent = document.getElementById("tweetContent")
+  const message = tweetContent.value;
+  
+  const container = document.getElementById("container");
+  container.style.minHeight = "200px";
+
+  const infoMessage = document.getElementById("infoMessage");
+  infoMessage.innerText = '';
+  
+  var noEmptyStringRegex = /^(?!\s*$).+/;
+  var match = noEmptyStringRegex.test(message);
+
+  if (match) {
+    makeRequest(message).then((res) => {
+      console.log(res)
+      if(res.ok){
+        uploadAnimation('succes-animate')
+        infoMessage.innerText = 'The Tweet was created successfully';
+        infoMessage.style.display = 'block';
+        tweetContent.innerText = '';
+      }else{
+        uploadAnimation('falied-animate')
+        infoMessage.innerText = 'An error occurred while trying to create the Tweet';
+        infoMessage.style.display = 'block';
+      }
+    }).catch((err) => {
+      uploadAnimation('falied-animate')
+      infoMessage.innerText = 'An error occurred while trying to create the Tweet';
+      infoMessage.style.display = 'block';
+      console.log("ERROR: ", err)
+    })
+  }else{
+    uploadAnimation('falied-animate')
+    infoMessage.innerText = 'Enter a valid message';
+    infoMessage.style.display = 'block';
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const btn = document.getElementById("sendTweetBtn");
+
+  btn.addEventListener("click", () => createTweet());
+});
